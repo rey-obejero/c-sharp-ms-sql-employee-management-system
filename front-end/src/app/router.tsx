6621809032fd08a900b585ceaab1b2cdc7fbd5b8
Rey Obejero <rey.obejero.mail@gmail.com>
2026-09-17 03:46:50 +0800

    feat(front-end): add employees route

--- front-end/src/app/router.tsx
+++ front-end/src/app/router.tsx
@@ -1,8 +1,16 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from 'react-router'
+
+import { Employees } from '@/app/routes/application/employees'
+import { ApplicationRoot } from '@/app/routes/application/root'
+import { paths } from '@/config/paths'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div></div>,
+    path: paths.root,
+    element: <ApplicationRoot />,
+    children: [
+      { index: true, element: <Navigate to={paths.employees} replace /> },
+      { path: paths.employees, element: <Employees /> },
+    ],
   },
-]);
+])
