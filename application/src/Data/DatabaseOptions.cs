9dc8ce8a05f426173fa4214fb84dec49ae00f975
Rey Obejero <rey.obejero.mail@gmail.com>
2026-09-17 07:31:49 +0800

    feat(application): register the data layer

--- application/src/Data/DatabaseOptions.cs
+++ application/src/Data/DatabaseOptions.cs
@@ -1,8 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
 namespace EmployeeManagementSystem.Data;
 
 public sealed class DatabaseOptions
 {
     public const string SectionName = "Database";
 
+    [ConfigurationKeyName("Initialize")]
     public bool ShouldInitialize { get; init; }
 }
